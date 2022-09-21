2c93a06f9e25829181eb083d4b7fc6db0243a1dc
Alexandre Pinho <aurale@gmail.com>
2022-09-21 20:29:04 -0300

    Commit Inicial do projeto spring boot com conexão mysql

---
+++ src/main/java/com/flowmoney/api/FlowmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.flowmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowmoney/api/FlowmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowmoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
