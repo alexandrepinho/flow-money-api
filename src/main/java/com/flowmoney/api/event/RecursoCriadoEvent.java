497b611133a2d0c5cd3740dfce34b518e8f2cb9b
Alexandre Pinho <aurale@gmail.com>
2022-09-23 23:49:39 -0300

    refactoring: ajuste para header location quando recurso é criado que servirá de base e organização para outros recursos

---
+++ src/main/java/com/flowmoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,28 @@
+package com.flowmoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = -5096374198515427728L;
+
+	private HttpServletResponse response;
+	private Long codigo;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+		super(source);
+		this.response = response;
+		this.codigo = codigo;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+}
