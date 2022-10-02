16ffb3ee174ae7077e08b7ad81d07bce5219f460
Alexandre Pinho <aurale@gmail.com>
2022-10-02 17:48:37 -0300

    feat: nome do usuário no jwt token

--- src/main/java/com/flowmoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/flowmoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.flowmoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -17,32 +21,28 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-	private AuthenticationManager authenticationManager;
-	
 	@Autowired
-	private UserDetailsService userDetailsService;	
-    	
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-	    clients.inMemory()
-	            .withClient("angular")
-	            .secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
-	            .scopes("read", "write")
-	            .authorizedGrantTypes("password", "refresh_token")
-	            .accessTokenValiditySeconds(1800)
-	            .refreshTokenValiditySeconds(3600 * 24);
+		clients.inMemory().withClient("angular").secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(1800).refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-			.authenticationManager(authenticationManager)
-			.accessTokenConverter(accessTokenConverter())
-			.tokenStore(tokenStore())
-			.userDetailsService(userDetailsService)
-			.reuseRefreshTokens(false);
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+		endpoints.authenticationManager(authenticationManager).accessTokenConverter(accessTokenConverter())
+				.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain).userDetailsService(userDetailsService)
+				.reuseRefreshTokens(false);
 	}
 
 	@Bean
@@ -59,5 +59,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		return new JwtTokenStore(accessTokenConverter());
 	}
 
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomTokenEnhancer();
+	}
 
 }
---
+++ src/main/java/com/flowmoney/api/security/UsuarioSistema.java
@@ -0,0 +1,29 @@
+package com.flowmoney.api.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.flowmoney.api.model.Usuario;
+
+public class UsuarioSistema extends User {
+
+	private static final long serialVersionUID = 1792137088821180702L;
+
+	private Usuario usuario;
+
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
